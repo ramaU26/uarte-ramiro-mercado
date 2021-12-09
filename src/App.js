eb15a6ed2e00cfde618662a05a16bb982808013b
ramaU26 <rama.uarte@gmail.com>
2021-12-09 19:03:57 -0300

    Buttom + y -

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import './App.css';
 import  BarraDeNavegacion from './componentes/NavBar'
 import ItemListContainer from './componentes/ItemListContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+import React , {useState} from 'react'
+import Button from './componentes/ClassButton/Button';
+
+
+
 
 function App() {
+  const [cantidad, setCantidad] = useState(0)
+  const sumar = () => {
+    if(cantidad < 50){setCantidad( cantidad + 1)}
+    
+  }
+  const restar = () => {
+    if(cantidad > 0){setCantidad( cantidad - 1)}
+  }
+  
+
   return ( 
     <React.Fragment className="app">
       <BarraDeNavegacion/>
       <ItemListContainer ></ItemListContainer>
+     <div></div>
+     <span>{cantidad}</span>
+     <button onClick={sumar}>+</button>
+     <button onClick={restar}>-</button>
      </React.Fragment>
     
     
